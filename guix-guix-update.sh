7263744bfc6c6ebdd626c7cfdaa9ba0b4ace4200
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-02-13 14:29:26 -0500

    update

--- guix-guix-update.sh
+++ guix-guix-update.sh
@@ -1,12 +1,16 @@
 
 # https://guix.gnu.org/manual/en/html_node/Upgrading-Guix.html#Upgrading-Guix
 
+#selinux
+sudo setenforce 0
+sestatus
+
 # On a foreign distro, you can upgrade the build daemon by running:
 sudo -i guix pull
 
 # systemctl restart ncsd
-systemctl restart guix-daemon
-systemctl restart gnu-store.mount 
+sudo systemctl restart guix-daemon
+sudo systemctl restart gnu-store.mount 
 
 # On Guix System, upgrading the daemon is achieved by reconfiguring 
 # the system (see guix system reconfigure). 
