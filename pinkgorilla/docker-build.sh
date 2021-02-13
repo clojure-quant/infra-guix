9f08c15d5ac820c2c92d3ee015d81268bb5c4a28
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-02-13 11:54:34 -0500

    docker run working

--- pinkgorilla/docker-build.sh
+++ pinkgorilla/docker-build.sh
@@ -1,16 +1,38 @@
 
+# see:
+# https://github.com/pjotrp/guix-notes/blob/master/CONTAINERS.org#docker
+
+# docker daemon has to be running
+# for problems with docjer socket see:
+# https://developer.fedoraproject.org/tools/docker/docker-installation.html
+
+echo "cleaning up artefacts directory"
 rm -R -f ./artefacts
 mkdir ./artefacts
 
+echo "guix is creating docker tar.gz file"
 guix system \
      --root=./artefacts/docker-pinkgorilla.tar.gz \
      docker-image \
      ./os.scm
-
-#     -m manifest.scm \
-
+#     --manifest=manifest.scm \
 #          --root=./container \
-#     --load-path=./scm  this is bad - crwates packages for all scm files
+#  -S /usr/bin=/bin 
+#  -S /etc/profile=/etc/profile \
+#  -S /book-evolutionary-genomics=/share/book-evolutionary-genomics \
+# -p ~/opt/book-evolutionary-genomics \
+#  --no-grafts 
+#  -i book-evolutionary-genomics \
+# --entry-point=PROGRAM
 
-mkdir ./artefacts/docker-pinkgorilla
-tar -xf ./artefacts/docker-pinkgorilla.tar.gz -C ./artefacts/docker-pinkgorilla
+
+# echo "uncompressing docker tar"
+# mkdir ./artefacts/docker-pinkgorilla
+# tar -xf ./artefacts/docker-pinkgorilla.tar.gz -C ./artefacts/docker-pinkgorilla
+
+echo "docker is loading docker image from tar.gz .."
+docker load --input ./artefacts/docker-pinkgorilla.tar.gz
+# image will have tag guix:latest
+
+echo "tagging image: pinkgorilla:latest"
+docker tag guix:latest pinkgorilla:latest
--- pinkgorilla/docker-run-interactive.sh
+++ pinkgorilla/docker-run-interactive.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
+# docker list-images
+
 # echo "NODE_ENV: " ${NODE_ENV}
 
-#sudo 
+
 docker run \
        -p 7070:7070 \
        --net "host" \
        -e NODE_ENV=${NODE_ENV} \
-       -t -i ./artefacts/docker-pinkgorilla \
+       -t -i pinkgorilla:latest \
        /bin/bash
