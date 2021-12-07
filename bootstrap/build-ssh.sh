74a1dbdbfe575990aea16851798abd00ef1c6ea1
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-12-07 22:25:57 +0100

    load path scm

--- bootstrap/build-ssh.sh
+++ bootstrap/build-ssh.sh
@@ -3,4 +3,4 @@
 echo "reconfiguring os config rock-ssh"
 
 export GUILE_LOAD_PATH=./modules:$GUILE_LOAD_PATH
-sudo guix system reconfigure ./machine/rock-ssh.scm
+sudo guix system reconfigure ../machine/rock-ssh.scm
