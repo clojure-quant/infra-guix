f00fa5ee9aeb3bfa8e4859811c12afe42fdfe45a
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-12-07 23:37:05 +0100

    sudo2

--- bootstrap/build-ssh.sh
+++ bootstrap/build-ssh.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-echo "reconfiguring os config rock-ssh"
-echo "PWD:" $PWD
-
-sudo export GUILE_LOAD_PATH=$PWD/modules:$GUILE_LOAD_PATH
-sudo echo "GUILE LOAD PATH:" $GUILE_LOAD_PATH
-
-sudo guix system reconfigure ./machine/rock-ssh.scm 
+sudo ./build-ssh.sh
