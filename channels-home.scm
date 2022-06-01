4918e286d41307dae52e69a465c2a50ab181bce8
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-06-01 06:52:15 +0200

    guix-science added

--- channels-home.scm
+++ channels-home.scm
@@ -17,9 +17,7 @@
           (make-channel-introduction "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))) 
 
-    ;  (channel
-    ;    (name 'science)
-    ;    (url "https://github.com/guix-science/guix-science.git")) ; Rstudio
+
 
       (channel
         (name 'gorilla)
@@ -42,6 +40,14 @@
         (name 'pantherx)
         (url "https://channels.pantherx.org/git/pantherx-extra.git")) ; vs-codium
 
+      (channel ; for python-bokeh, ; Rstudio
+        (name 'guix-science)
+        (url "https://github.com/guix-science/guix-science.git")
+        (introduction
+         (make-channel-introduction
+              "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+              (openpgp-fingerprint
+               "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
       
   )
   
