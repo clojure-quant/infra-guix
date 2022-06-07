ae93fe93f86b01868dddbd8c6a8b55f04f973de1
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-06-07 03:18:53 +0200

    r-install moved to pink-gorilla/studio

--- OLD/old-python-r/install-r/installRpackages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# because it will install dependencies globally it needs to be run as sudo
-
-R -e 'install.packages("DT" , repos="http://cran.us.r-project.org")'
-R -e 'install.packages("ggplot2" , repos="http://cran.us.r-project.org")'
-R -e 'install.packages("svglite" , repos="http://cran.us.r-project.org")'
-R -e 'install.packages("plotly" , repos="http://cran.us.r-project.org")'
