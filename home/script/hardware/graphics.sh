becc686791f6b14ea509922d494177f1d0a92448
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-10-25 04:26:43 +0200

    graphics info

--- home/script/hardware/graphics.sh
+++ home/script/hardware/graphics.sh
@@ -11,16 +11,27 @@ cat /sys/class/drm/card0-DP-1/modes
 
 
 # guix install edid-decode
-
+# this shows A LOT of info
+cat /sys/class/drm/card0-DP-1/edid |edid-decode
+# 3840x2160   30.000 Hz  16:9 
 
 
 #If you want to list all the modes the compositor supports (might not be the same as your monitor), you can run weston-info
-
-The simplest way is to check the weston log which will give you all supported modes for each connector(VGA,LVDS and HDMI)
-
-
-first get my monitor information using this command:
-
+# The simplest way is to check the weston log which will give you all supported modes for each connector(VGA,LVDS and HDMI)
+# first get my monitor information using this command:
 swaymsg -t get_outputs
+# Current mode: 3840x2160 @ 60.000 Hz
 
-swaymsg output DP-3 mode 3440x1440@144Hz position 0 0
+
+# set mode..
+# swaymsg output DP-3 mode 3440x1440@144Hz position 0 0
+
+lshw -c video
+
+# this shows intel driver:
+lspci -k | grep -EA3 'VGA|3D|Display'
+
+
+# Enlightenment has a new display manager called Entrance,
+#  which is provided by the entrance-gitAUR package. Entrance is quite sophisticated and its configurat
+
